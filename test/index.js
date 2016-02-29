edddf7c3040da9ba821165d3a7355b00340f5418
Johannes Stein <johannesstein@freeze-dev.com>
2016-02-29 22:31:58 +0100

    Update tests

--- test/index.js
+++ test/index.js
@@ -2,11 +2,35 @@ import React from 'react';
 import { expect } from 'chai';
 import BackgroundVideo from 'react-background-video';
 
-import { render } from 'enzyme';
+import { render, mount } from 'enzyme';
 
 describe('<BackgroundVideo />', () => {
   it('is instantiable', () => {
     const wrapper = render(<BackgroundVideo />);
     expect(wrapper).to.not.equal(null);
+  });
+
+  it('passes in videos', () => {
+    const videos = [{
+      src: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+      type: 'mp4',
+    }];
+
+    const wrapper = render(<BackgroundVideo videos={videos} />);
+    expect(wrapper).to.not.equal(null);
+  });
+
+  it('calls `componentWillUnmount` when unmounting', () => {
+    sinon.spy(BackgroundVideo.prototype, 'componentWillUnmount');
+
+    const videos = [{
+      src: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+      type: 'mp4',
+    }];
+
+    const wrapper = mount(<BackgroundVideo videos={videos} />);
+    expect(BackgroundVideo.prototype.componentWillUnmount.calledOnce).to.equal(false);
+    wrapper.unmount();
+    expect(BackgroundVideo.prototype.componentWillUnmount.calledOnce).to.equal(true);
   });
 });
